390ea5c762df5917ef42645bb11198792f46f2ba
isabelcooper <isabel@triptease.com>
2020-02-02 15:09:53 +0000

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt-get install gettext
+
+envsubst < app.yaml.template > app.yaml
+
+if [[ ! $(command -v gcloud) ]]; then
+    echo 'Installing gcloud sdk'
+    sudo apt-get install python
+    wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-214.0.0-linux-x86_64.tar.gz
+    tar xzf google-cloud-sdk-214.0.0-linux-x86_64.tar.gz
+    ./google-cloud-sdk/install.sh -q
+    sudo cp -R google-cloud-sdk/* /usr/local
+fi
+
+echo ${GCLOUD_SERVICE_KEY} | base64 --decode > ${HOME}/ivycode-service-key.json
+gcloud auth activate-service-account --key-file=${HOME}/ivycode-service-key.json
+gcloud config set project ${GOOGLE_PROJECT_ID}
+gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
+gcloud auth list
+
+gcloud app deploy --project=${GOOGLE_PROJECT_ID} --quiet
